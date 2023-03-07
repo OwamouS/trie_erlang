595d3eb3f1c7c469072178f23b5c2c399215efe3
OwamouS <61242293+OwamouS@users.noreply.github.com>
2023-03-07 15:26:07 +0300

    Update trie.erl

--- src/trie.erl
+++ src/trie.erl
@@ -13,6 +13,36 @@
 
 -include("trie.hrl").
 
+% Adds or updates an object with the given key and returns the new trie.
+%store ([], Object, {TrieDict, _}) ->
+%	{TrieDict, Object};
+%store ([Elem | StringTail], Object, {TrieDict, Slot}) ->
+%	case orddict:find (Elem, TrieDict) of
+%		{ok, Subtrie} ->
+%			{orddict:store (Elem, store (StringTail, Object, Subtrie), TrieDict), Slot};
+%		error ->
+%			{orddict:store (Elem, store (StringTail, Object, new ()), TrieDict), Slot}
+%	end.
+%
+% Get the next trie from the key.
+%next ([], {TrieDict, Slot}) ->
+%	{TrieDict, Slot};
+%next ([Elem | StringTail], {TrieDict, _}) ->
+%	case orddict:find (Elem, TrieDict) of
+%		{ok, Subtrie} -> 
+%			next (StringTail, Subtrie);
+%		error -> undefined
+%	end.
+%
+% Get value of key.
+%value(Key, Trie) ->
+%	case next(Key, Trie) of
+%		{_, Value} ->
+%			Value;
+%		_ ->
+%			undefined
+%	end.
+
 new_trie() ->
   #node{}.
 
