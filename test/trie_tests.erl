2c0e08db6ce2147a2c1fffb2f3ce3c638ff8c921
OwamouS <alexandrlmashkovtsev@niuitmo.ru>
2023-03-17 19:34:24 +0300

    tests

--- test/trie_tests.erl
+++ test/trie_tests.erl
@@ -7,49 +7,72 @@ trie_erase/2,
 trie_search/2,
 trie_filter/2,
 trie_map/2,
-foldl/3,
-foldr/3,
+trie_foldl/3,
+trie_foldr/3,
 trie_merge/2
 ]).
 
 -include_lib("eunit/include/eunit.hrl").
 
-trie_append_erase_test() ->
-  % Test empty trie
-  ?assertEqual(undefined, trie:trie_search([], trie:new_trie())),
-  %
-  T1 = trie:trie_append("a", 1, trie:new_trie()),
-  ?assertEqual(1, trie:trie_search("a", T1)),
-  T2 = trie:trie_append("b", 2, T1),
-  ?assertEqual(2, trie:trie_search("b", T2)),
-  %%
-  T3 = trie:trie_erase("b", T2),
-  ?assertEqual(undefined, trie:trie_search("b", T3)).
+%% Insert/Search
+insert_search_test() ->
+  _Trie = trie:trie_append("a", 42, trie:new_trie()),
+  _Trie1 = trie:trie_append("bb", 43, _Trie),
+  ?assertEqual(43, trie:trie_search("bb", _Trie1)),
+  ?assertEqual(42, trie:trie_search("a", _Trie)).
 
-trie_merge_test() ->
-  Node1 = trie_append("foo", 1, new_trie()),
-  Node2 = trie_append("bar", 2, Node1),
-  Node3 = trie_append("baz", 3, Node2),
+%% Erase
+erase_test() ->
+  Trie1 = trie:trie_append("a", 42, trie:new_trie()),
+  Trie2 = trie:trie_erase("a", Trie1),
+  ?assertNotEqual(Trie1, Trie2).
 
-  % create tree 2
-  Node4 = trie_append("foo", 4, new_trie()),
-  Node5 = trie_append("qux", 5, Node4),
+%% Merge
+merge_test() ->
+  Trie1 = trie:trie_append("a", 42, trie:new_trie()),
+  Trie2 = trie:trie_append("b", 43, trie:new_trie()),
+  Trie3 = trie:trie_merge(Trie1, Trie2),
+  ?assertEqual(42, trie:trie_search("a", Trie3)),
+  ?assertEqual(43, trie:trie_search("b", Trie3)).
 
-  % merge the two trees
-  Merged = trie_merge(Node3, Node5),
+%% Filter
+filter_test() ->
+  Trie = trie:trie_append("a", 42, trie:new_trie()),
+  Trie1 = trie:trie_append("b", -42, Trie),
 
-  % check that the merged tree has all the expected values
-  ?assertEqual(1, trie_search("foo", Merged)),
-  ?assertEqual(2, trie_search("bar", Merged)),
-  ?assertEqual(3, trie_search("baz", Merged)),
-  ?assertEqual(5, trie_search("qux", Merged)).
+  Trie2 = trie:trie_filter(fun(V) -> V > 0 end, Trie1),
+  ?assertEqual(Trie, Trie2).
+
+%% Map
+map_test() ->
+  Trie = trie:trie_append("a", 42, trie:new_trie()),
+  Trie1 = trie:trie_append("b", -42, Trie),
+
+  Trietest = trie:trie_append("a", 84, trie:new_trie()),
+  Trietest2 = trie:trie_append("b", -84, Trietest),
+  Trie2 = trie:trie_map(fun(V) -> V * 2 end, Trie1),
+  ?assertEqual(Trietest2, Trie2).
+
+%% Foldr/Foldl
+foldr_foldl_test() ->
+  Trie = trie:trie_append("a", 42, trie:new_trie()),
+
+  Acc1 = trie:trie_foldr(fun(V, Acc) -> V + Acc end, 0, Trie),
+  Acc2 = trie:trie_foldl(fun(Acc, V) -> V + Acc end, 0, Trie),
+
+  Acc3 = trie:trie_foldl(fun(Acc, V) -> V + Acc end, 0, trie:trie_filter(fun(V) -> V > 0 end, Trie)),
+  Acc4 = trie:trie_foldl(fun(Acc, V) -> V + Acc end, 0, trie:trie_map(fun(V) -> V * 2 end, Trie)),
+  ?assertEqual(Acc1, 42),
+  ?assertEqual(Acc2, 42),
+  ?assertEqual(Acc3, 42),
+  ?assertEqual(Acc4, 84).
 
 trie_monoid_test() ->
   ?assertEqual(new_trie(), trie_merge(new_trie(), new_trie())),
   ?assertEqual(trie_merge(new_trie(), new_trie()), new_trie()),
-  Tree1 = trie_append("hello", "world", new_trie()),
-  Tree2 = trie_append("goodbye", "everyone", new_trie()),
-  Tree3 = trie_append("morning", "sir", new_trie()),
+  Tree1 = trie_append("a", "b", new_trie()),
+  Tree2 = trie_append("c", "d", new_trie()),
+  Tree3 = trie_append("e", "f", new_trie()),
   ?assertEqual(trie_merge(Tree1, trie_merge(Tree2, Tree3)), trie_merge(trie_merge(Tree1, Tree2), Tree3)),
   ?assertEqual(trie_merge(Tree2, trie_merge(Tree1, Tree3)), trie_merge(trie_merge(Tree1, Tree2), Tree3)),
   ?assertEqual(trie_merge(Tree3, trie_merge(Tree1, Tree2)), trie_merge(trie_merge(Tree1, Tree2), Tree3)).
